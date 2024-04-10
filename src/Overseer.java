eff584c5efd5b0c10190414b9a538e8f87af6fd3
fnc8822 <81328370+fnc8822@users.noreply.github.com>
2024-04-10 20:52:34 -0300

    Initial version

---
+++ src/Overseer.java
@@ -0,0 +1,160 @@
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
+
+///// JPANEL CLASS (DRAWS GRAPHICS, LISTENS FOR KEY INPUT, CALLS FOR MOVES)
+public class Overseer extends JPanel implements KeyListener {
+
+    private Cannon ship; // game elements generated in main class
+    private AlienMan enemies;
+    private Bullet shot; // bullet shot by user
+    private Scorekeeper scoreMan;
+    private Shield shield;
+    private BulletMan shotsFired;
+
+    private boolean[]keys; // holds keyboard input
+
+    private boolean playing = true; // flag to see if game is still ongoing
+    private boolean paused = false; // flag for when user pauses/unpauses game
+    private boolean restartGame = false; // flag for when user wants to restart the game
+
+    private Color pOverlay = new Color(0, 0, 0, 200);
+    private File ttf = new File("fonts/visitor.ttf"); // font used to draw score, etc.
+    private Font fontL = Font.createFont(Font.TRUETYPE_FONT,ttf).deriveFont(Font.PLAIN,150); // various font sizes
+    private Font fontM = Font.createFont(Font.TRUETYPE_FONT,ttf).deriveFont(Font.PLAIN,100);
+    private Font fontS = Font.createFont(Font.TRUETYPE_FONT,ttf).deriveFont(Font.PLAIN,40);
+
+    public Overseer(Cannon player, AlienMan badGuys, Scorekeeper getScore, Shield getShield, BulletMan getShots) throws IOException, FontFormatException{
+        super();
+        keys = new boolean[KeyEvent.KEY_LAST+1];
+        ship = player;
+        enemies = badGuys;
+        scoreMan = getScore;
+        shield = getShield;
+        shotsFired = getShots;
+
+        setSize(770,652);
+        addKeyListener(this);
+    }
+
+    public void addNotify(){
+        super.addNotify();
+        requestFocus();
+    }
+
+    public void move(){ // takes in keyboard input and moves user cannon
+        if (keys[KeyEvent.VK_RIGHT] && (ship.getPos() + 5) <= 740) {
+            ship.right();
+        }
+        if (keys[KeyEvent.VK_LEFT] && (ship.getPos() - 5) >= 12) {
+            ship.left();
+        }
+        if (keys[KeyEvent.VK_SPACE] && shotsFired.playerCanShoot()) {
+            // canShoot flag prevents user from shooting infinite bullets one after another
+            shotsFired.setPlayerShot(new Bullet(ship.getPos(), 556, Bullet.UP));
+
+            // play music
+            try {
+                SoundMan.play("playerShoot");
+            } catch (UnsupportedAudioFileException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (LineUnavailableException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // draws the background
+    private void backDraw(Graphics g){
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,770,652);
+        g.setColor(Color.GREEN);
+        g.fillRect(0,32,770,3);
+        scoreMan.draw(g);
+    }
+
+    // draws the pause overlay
+    private void pauseOverlay(Graphics g){
+        g.setColor(pOverlay);
+        g.fillRect(0,0,770,652);
+        Graphics2D comp2D = (Graphics2D)g;
+        g.setColor(Color.WHITE);
+        comp2D.setFont(fontL);
+        comp2D.drawString("PAUSED",120,320);
+    }
+
+    // draws text for game over screen
+    private void gameOverOverlay(Graphics g){
+        Graphics2D comp2D = (Graphics2D)g;
+        g.setColor(Color.WHITE);
+        comp2D.setFont(fontM);
+        comp2D.drawString("GAME OVER",120,320);
+        comp2D.setFont(fontS);
+        comp2D.drawString("PRESS P TO PLAY AGAIN",130,400);
+    }
+
+    // called to see if game is still in play
+    public boolean stillPlaying(){
+        if (ship.getLives() <= 0){ // check if player is still alive
+            playing = false;
+        }
+        if (enemies.reachedBottom()){
+            playing = false;
+        }
+        return playing;
+    }
+
+    public boolean isPaused(){
+        return paused;
+    }
+
+    public boolean doRestartGame(){
+        return restartGame;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e){} // keyboard listeners
+
+    @Override
+    public void keyPressed(KeyEvent e){ keys[e.getKeyCode()] = true; }
+
+    @Override
+    public void keyReleased(KeyEvent e){
+        keys[e.getKeyCode()] = false;
+
+        // pause game
+        if (e.getKeyCode() == KeyEvent.VK_P && playing){
+            paused = !paused;
+        }
+
+        // restart game when player has lost
+        if (e.getKeyCode() == KeyEvent.VK_P && !playing){
+            restartGame = true;
+        }
+    }
+
+    public void paintComponent(Graphics g){ // paints all elements of screen
+        backDraw(g);
+        if (playing){ // while game is still ongoing
+            shield.draw(g);
+            ship.draw(g);
+            shotsFired.draw(g);
+               enemies.draw(g);
+        }
+        else { // when game is over
+            gameOverOverlay(g);
+        }
+
+        if (paused){
+            pauseOverlay(g);
+        }
+    }
+
+}
---
+++ src/SpaceInvaders.java
@@ -0,0 +1,168 @@
+/*
+========================================
+Space Invaders Recreated
+by Lloyd Torres
+
+Published on February 2013
+Updated on July 2014
+
+Space Invaders is copyrighted by Taito Corporation.
+Code provided by Lloyd on the GNU GPL v3.0 license.
+========================================
+Legal:
+
+Copyright (C) 2014 Lloyd Torres
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+========================================
+*/
+
+///// MODULES TO IMPORT
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+///// MAIN CLASS
+public class SpaceInvaders extends JFrame implements ActionListener{
+
+    private javax.swing.Timer myTimer;
+
+    private MainMenu menu; // first JPanel
+    private boolean gameStart = false;
+
+    private Overseer overseer; // component classes of the game
+    private Cannon player;
+    private AlienMan enemies;
+    private Scorekeeper scoreMan;
+    private Shield shield;
+    private BulletMan shotsFired;
+    private int wave = 0; // # of wins by the user, keeps track of subsequent alien start location
+    
+    public SpaceInvaders() throws IOException, FontFormatException{
+        super("Space Invaders Recreated");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        setLayout(null);
+        setSize(770,652);
+
+        menu = new MainMenu();
+        scoreMan = new Scorekeeper();
+
+        menu.setHiScore(scoreMan.getHiScore());
+
+        add(menu);
+
+        myTimer = new javax.swing.Timer(10,this); // update every 10 ms
+        myTimer.start();
+        
+        setResizable(false);
+        setVisible(true);
+    }
+    
+    private void nextLevel() throws IOException, FontFormatException { // called every time user wins (all aliens destroyed), resets game setup
+        remove(overseer);
+        if (wave < 10){
+            wave += 1;
+        }
+        player.addLife();
+        enemies = new AlienMan(wave,scoreMan,player,shield);
+        shotsFired = new BulletMan(player,enemies,shield);
+        overseer = new Overseer(player,enemies,scoreMan,shield,shotsFired);
+        add(overseer);
+    }
+
+    private void startOverGame() throws IOException, FontFormatException {
+
+        if (overseer != null){
+            remove(overseer);
+        }
+
+        wave = 0;
+        player = new Cannon();
+        shield = new Shield();
+        scoreMan.setShip(player);
+        scoreMan.resetScore();
+        enemies = new AlienMan(wave,scoreMan,player,shield);
+        shotsFired = new BulletMan(player,enemies,shield);
+        overseer = new Overseer(player,enemies,scoreMan,shield,shotsFired);
+        add(overseer);
+    }
+    
+    public void actionPerformed(ActionEvent evt){ // event listener stuff, update classes every 10 ms
+        Object source = evt.getSource();
+        if(source == myTimer){
+            if (gameStart) {
+                if (overseer.stillPlaying() && !overseer.isPaused() && !player.gotHit()) { // only move when not paused and player still alive
+                    overseer.move(); // move player
+                    if (enemies.metronome()) { // if aliens have moved
+                        shotsFired.setAlienShots(enemies.attack()); // launch attack
+                    }
+                    enemies.ufoTrack(); // move mystery UFO regardless of beat
+                    shotsFired.trackBullets(); // move shots if they exist
+                }
+
+                if (!overseer.stillPlaying()) {
+                    enemies.ufoDestroy();
+                    scoreMan.calculateHiScore();
+                }
+
+                overseer.repaint();
+
+                if (enemies.aliensGone()) { // if no aliens left
+                    // error handling in case font doesn't exist
+                    try {
+                        nextLevel();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (FontFormatException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                if (overseer.doRestartGame()) { // check if player wants to restart game
+                    // error handling in case font doesn't exist
+                    try {
+                        startOverGame();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (FontFormatException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            else {
+                gameStart = menu.getStatus();
+                if (gameStart){ // initialize if player starts game
+                    remove(menu);
+
+                    // error handling in case font doesn't exist
+                    try {
+                        startOverGame();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (FontFormatException e) {
+                        e.printStackTrace();
+                    }
+                }
+                menu.repaint();
+            }
+        }
+    }
+    
+    public static void main(String[]args) throws IOException, FontFormatException{
+        new SpaceInvaders();
+    }
+}
